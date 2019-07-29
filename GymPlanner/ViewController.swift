16dc68216c594c37dc0993388809e5efe892bd65
Liam <liamlogan1996@outlook.com>
2019-07-30 00:27:13 +0100

    Initial Commit

---
+++ GymPlanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GymPlanner
+//
+//  Created by Liam Logan on 30/07/2019.
+//  Copyright © 2019 Liam Logan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
